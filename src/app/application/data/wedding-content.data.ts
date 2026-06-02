02b504b50577c0816083266927b159e19e45e756
KaioQuaresmadS <116770886+KaioQuaresmadS@users.noreply.github.com>
2026-06-02 15:45:43 -0300

    Update wedding-content.data.ts

--- src/app/application/data/wedding-content.data.ts
+++ src/app/application/data/wedding-content.data.ts
@@ -21,9 +21,13 @@ export const WEDDING_INFO: WeddingInfo = {
     },
     {
       label: 'Mensagem',
-      title: 'ECLESIASTES 4:10-12',
+      title: 'Convite',
       description:
-        'Queremos convidar você para o nosso casamento!!\nA cerimônia acontecerá no templo onde nos juntamos para celebrar a Deus e desejamos que esteja conosco nesse dia especial.\n\n10 Porque se um cair, o outro levanta o seu companheiro; mas ai do que estiver só; pois, caindo, não haverá outro que o levante.\n11 Também, se dois dormirem juntos, eles se aquentarão; mas um só, como se aquentará?\n12 E, se alguém prevalecer contra um, os dois lhe resistirão; e o cordão de três dobras não se quebra tão depressa.'
+        "Com grande alegria, queremos convidar você para celebrar conosco um dos momentos mais importantes de nossas vidas. Nossa cerimônia acontecerá na igreja onde congregamos e servimos ao Senhor, um lugar que faz parte da nossa caminhada de fé e onde desejamos receber a bênção de Deus para esta nova etapa.Será uma honra ter você conosco nesse dia tão especial, compartilhando esse momento de amor, gratidão e alegria diante de Deus.
+
+"Porque, se um cair, o outro levanta o seu companheiro; mas ai do que estiver só, pois, caindo, não haverá quem o levante.Também, se dois dormirem juntos, eles se aquentarão; mas um só, como se aquentará?E, se alguém prevalecer contra um, os dois lhe resistirão; e o cordão de três dobras não se quebra com facilidade."
+
+      Eclesiastes 4:10-12"
     }
   ]
 };
