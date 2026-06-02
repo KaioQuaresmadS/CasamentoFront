adda8e65664abd92ddf0c2af80b809b3f6e0996b
KaioQuaresmadS <116770886+KaioQuaresmadS@users.noreply.github.com>
2026-06-02 15:46:56 -0300

    Refactor wedding invitation message formatting
    Removed line breaks from wedding invitation message.

--- src/app/application/data/wedding-content.data.ts
+++ src/app/application/data/wedding-content.data.ts
@@ -23,11 +23,7 @@ export const WEDDING_INFO: WeddingInfo = {
       label: 'Mensagem',
       title: 'Convite',
       description:
-        "Com grande alegria, queremos convidar você para celebrar conosco um dos momentos mais importantes de nossas vidas. Nossa cerimônia acontecerá na igreja onde congregamos e servimos ao Senhor, um lugar que faz parte da nossa caminhada de fé e onde desejamos receber a bênção de Deus para esta nova etapa.Será uma honra ter você conosco nesse dia tão especial, compartilhando esse momento de amor, gratidão e alegria diante de Deus.
-
-"Porque, se um cair, o outro levanta o seu companheiro; mas ai do que estiver só, pois, caindo, não haverá quem o levante.Também, se dois dormirem juntos, eles se aquentarão; mas um só, como se aquentará?E, se alguém prevalecer contra um, os dois lhe resistirão; e o cordão de três dobras não se quebra com facilidade."
-
-      Eclesiastes 4:10-12"
+        "Com grande alegria, queremos convidar você para celebrar conosco um dos momentos mais importantes de nossas vidas. Nossa cerimônia acontecerá na igreja onde congregamos e servimos ao Senhor, um lugar que faz parte da nossa caminhada de fé e onde desejamos receber a bênção de Deus para esta nova etapa.Será uma honra ter você conosco nesse dia tão especial, compartilhando esse momento de amor, gratidão e alegria diante de Deus.'Porque, se um cair, o outro levanta o seu companheiro; mas ai do que estiver só, pois, caindo, não haverá quem o levante.Também, se dois dormirem juntos, eles se aquentarão; mas um só, como se aquentará?E, se alguém prevalecer contra um, os dois lhe resistirão; e o cordão de três dobras não se quebra com facilidade.'Eclesiastes 4:10-12"
     }
   ]
 };
