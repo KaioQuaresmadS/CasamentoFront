a072ee5bec43e810722fb4ef8efc27f3958f40f8
KaioQuaresmadS <116770886+KaioQuaresmadS@users.noreply.github.com>
2026-06-03 16:46:08 +0000

    Feito alteração Convite

--- src/app/application/data/wedding-content.data.ts
+++ src/app/application/data/wedding-content.data.ts
@@ -23,7 +23,7 @@ export const WEDDING_INFO: WeddingInfo = {
       label: 'Mensagem',
       title: 'Convite',
       description:
-        'Com grande alegria, queremos convidar você para celebrar conosco um dos momentos mais importantes de nossas vidas. Nossa cerimônia acontecerá na igreja onde congregamos e servimos ao Senhor, um lugar que faz parte da nossa caminhada de fé e onde desejamos receber a bênção de Deus para esta nova etapa.Será uma honra ter você conosco nesse dia tão especial, compartilhando esse momento de amor, gratidão e alegria diante de Deus. Eclesiastes 4:10-12'
+        'Com grande alegria, queremos convidar você para celebrar conosco um dos momentos mais importantes de nossas vidas. Nossa cerimônia acontecerá na igreja onde congregamos e servimos ao Senhor, um lugar que faz parte da nossa caminhada de fé e onde desejamos receber a bênção de Deus para esta nova etapa. Será uma honra ter você conosco nesse dia tão especial, compartilhando esse momento de amor, gratidão e alegria diante de Deus. Eclesiastes 4:10-12'
     }
   ]
 };
